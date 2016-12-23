ff11c22772b48881659b3ad6bd740888e2fbc0e4
rkaranam <karanam.rajasekhar44@gmail.com>
2016-12-23 19:04:37 +0530

    Introduction to Lambdas

--- src/org/learnjava/lambdas/Greeter.java
+++ src/org/learnjava/lambdas/Greeter.java
@@ -20,11 +20,30 @@ public class Greeter {
 		greeter.greet();
 		greeter.hello();
 		
+		// Passing behavior in OOP
 		ChristmasGreeting christmasGreeting = new ChristmasGreeting();
 		greeter.greet(christmasGreeting);
 		
 		NewYearGreeting newYearGreeting = new NewYearGreeting();
 		greeter.greet(newYearGreeting);
-	}
+		
+		GreetLambda greetLambdaFunction = () -> System.out.println("Hello, Lambda Expressions!");
+		greetLambdaFunction.foo();
+		
+		Greeting greetingFunctionInnerClass = new Greeting() {
+			@Override
+			public void perform() {
+				System.out.println("Hello, Annonymous Inner Class!");
+			}
+		};
+		greetingFunctionInnerClass.perform();
+		
+		Greeting greetingFunction = () -> System.out.println("Enjoy your work!");
+		greetingFunction.perform();
+	}	
 
+}
+
+interface GreetLambda {
+	void foo();
 }
